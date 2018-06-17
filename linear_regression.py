Neural network forward propagation added, need to resolve backpropagation bug

linear_regression.py
@@ -1,9 +1,17 @@
 from sklearn import datasets
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 
 diabetes = datasets.load_diabetes()
-print diabetes.data.shape, diabetes.target.shape
+print "(Data shape) (Labels shape) = ", diabetes.data.shape, diabetes.target.shape
+print "Label distribution: "
+unique_labels, label_freqs = np.unique(diabetes.target, return_counts=True)
+for label_idx in range(unique_labels.size):
+	if label_freqs[label_idx] != 0:
+		print unique_labels[label_idx], ": ", label_freqs[label_idx]
 
 '''
 TODO: Encapsulate below lin. reg. functions into a class 
@@ -12,7 +20,7 @@ class SimpleLinearRegression:
 
 '''
 
-def fit_model(X, y, w = None, learning_rate = 0.25, threshold_value = 0.001, error_statistic = 'RSS'):
+def fit_model(X, y, w = None, learning_rate = 0.25, threshold_value = 0.001):
 	ones_vector = np.ones((X.shape[0],1))	# Prepend samples with bias feature of ones
 	X = np.concatenate( (ones_vector, X), axis=1)
 	if w is None:
@@ -24,44 +32,44 @@ def fit_model(X, y, w = None, learning_rate = 0.25, threshold_value = 0.001, err
 		updated_weights = np.zeros(w.shape[0])
 		for w_i in range(w.shape[0]):
 			updated_weights[w_i] = current_weights[w_i] - (learning_rate / n_samples) * np.sum( np.multiply((X.dot(current_weights) - y), X[:,w_i])) 
-		if np.all( ((current_weights - updated_weights) < threshold_value)):
+		if np.all( ((np.absolute(current_weights - updated_weights)) < threshold_value)):
 			return updated_weights
 		else:
 			current_weights = updated_weights
 
 
 '''
-Prediction function that returns both the mean squared error over the test sample space and the individual
-predictions for each sample.
+Prediction function that returns both the mean squared error over the test sample space, individual
+predictions for each sample, and rounded predictions for each sample to match integer label values.
 '''
 def model_predict(X, y, w):
 	ones_vector = np.ones((X.shape[0],1))
 	X = np.concatenate( (ones_vector, X), axis=1)
 	num_samples = X.shape[0]
-	return (np.sum(np.square(( X.dot(w) - y ))) / num_samples, X.dot(w))
+	return (np.sum(np.square(( X.dot(w) - y ))) / num_samples, X.dot(w), np.around(X.dot(w)))
 
 '''
 TESTING CUSTOM LinearRegression MODEL
 '''
-from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(diabetes.data, diabetes.target, test_size = 0.2)
 
 custom_weights = fit_model(X_train, y_train)
-test_acc, test_preds = model_predict(X_test, y_test, custom_weights)
+test_acc, test_preds, rounded_preds = model_predict(X_test, y_test, custom_weights)
 print 'Custom linear regression model prediction MSE = ', test_acc
-print 'Custom linear regression model predictions: ', test_preds[1:20]
+print 'Custom linear regression model predictions: ', test_preds
+
+# print "Custom model accuracy with rounded prediction labels: {}".format(np.count_nonzero(rounded_preds == y_test)/rounded_preds.size)
+
 
 '''
 TESTING SKLEARN LinearRegression MODEL
 '''
-from sklearn.linear_model import LinearRegression
 
 reg_clf = LinearRegression()
 reg_clf.fit(X_train, y_train)
 
 sklearn_preds = reg_clf.predict(X_test)
-print 'Sklearn linear regression model predictions: ', sklearn_preds[1:20]
-
 print 'Sklearn linear regression model prediction MSE = ', np.sum(np.square( sklearn_preds - y_test )) / X_test.shape[0]
-
+print 'Sklearn linear regression model predictions: ', sklearn_preds
+# print 'Sklearn linear regression model rounded prediction accuracy: ', np.count_nonzero(np.around(sklearn_preds) == y_test)/sklearn_preds.size

logistic_regression.py
@@ -5,17 +5,19 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
+np.set_printoptions(threshold=np.nan)
+
 def sigmoid(x):
 	return np.reciprocal(1 + np.exp(-x))
-	#return  np.array([1]) / np.array(1 + math.exp(-x))
 
 '''
-Trains a set of n models for to learn each of the dataset labels and returns n sets of learned weights.
+Trains a set of n models to learn each of the dataset labels and returns n sets of learned weights.
 '''
-def fit_model(X, y, w = None, learning_rate = 0.25, threshold_value = 0.001, reg_coef = 1.0):
+def fit_model(X, y, w = None, learning_rate = 0.25, threshold_value = 0.01, reg_coef = 1):
 	unique_labels = np.unique(y)
 	# Create initial weights for each model to train
 	if w is None:
+		print "Providing default weights of ones"
 		w = np.ones((X.shape[1] + 1, unique_labels.shape[0]))
 
 	X = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
@@ -23,16 +25,20 @@ def fit_model(X, y, w = None, learning_rate = 0.25, threshold_value = 0.001, reg
 	num_samples = X.shape[0]
 	
 	for label_idx in range(unique_labels.shape[0]):
-		print "Training model for label: ", unique_labels[label_idx]
+		print "Now building model for label ", unique_labels[label_idx], "..."
+		# print "Training model for label: ", unique_labels[label_idx]
 		# Set labels to {0,1}; 1 = current model's label, 0 = all other labels 
 		y_curr_model = np.array([y == unique_labels[label_idx]]).astype(int)
 		current_weights = w[:, label_idx]
-		
+
 		while True:
 			current_model_errors = (sigmoid(np.dot(X,current_weights)) - y_curr_model).transpose()[:,0]
 			updated_weights = current_weights - (learning_rate/num_samples) * np.dot(X.transpose(),current_model_errors)
 			updated_weights[1:] -= (learning_rate*reg_coef/num_samples) * current_weights[1:] # Regularize non-bias weights (L2-norm)
-			if np.all( ((current_weights - updated_weights) < threshold_value)):
+			if np.all( ((np.absolute(current_weights - updated_weights)) < threshold_value)):
+				print "Finished building model for label ", unique_labels[label_idx]
+				print "Current weights: ", current_weights
+				print "Updated (final) weights: ", updated_weights
 				w[:, label_idx] = updated_weights	# Set weights for the current model and continue to next model
 				break
 			else:
@@ -50,18 +56,22 @@ digits_dataset = datasets.load_digits()
 X,y = digits_dataset.data, digits_dataset.target
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
 
+print "Training set labels (1-50): ", y_train[0:50]
+
 opt_weights = fit_model(X_train, y_train)
+
 predictions = model_predict(X_test, y_test, opt_weights)
 
 correct_preds_bool_arr = (predictions == y_test)
 
-
-#print "Custom model intercepts: ", opt_weights[0,:]
-#print "Custom model coef: ", opt_weights[1:,:]
-print "Custom model accuracy on digits dataset (multiclass dataset): {}".format(np.count_nonzero(correct_preds_bool_arr)/predictions.shape[0])
-
 log_reg_cfr = LogisticRegression()
 log_reg_cfr.fit(X_train, y_train)
-#print "Sklearn model intercepts: ", log_reg_cfr.intercept_
-#print "Sklearn model coef: ", log_reg_cfr.coef_
+
+print "Custom model intercepts: ", opt_weights[0,:]
+print "Sklearn model intercepts: ", log_reg_cfr.intercept_
+
+print "Custom model coef: ", opt_weights[1:,:]
+print "Sklearn model coef: ", log_reg_cfr.coef_
+
+print "Custom model accuracy on digits dataset (multiclass dataset): {}".format(np.count_nonzero(correct_preds_bool_arr)/predictions.shape[0])
 print "Sklearn model accuracy on digits dataset (multiclass dataset): {}".format(log_reg_cfr.score(X_test,y_test))